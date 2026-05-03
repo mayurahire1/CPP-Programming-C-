6b36c05db1973b41c441581840456171ed3bc05f
Mayur Ahire <162018537+mayurahire1@users.noreply.github.com>
2026-05-03 12:18:56 +0530

    Add files via upload

---
+++ EncapsulationDemoPrivate.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+//Encapsulation
+class Marvellous
+{  
+    // Access Specifier (By Default- Private)
+    
+    int No1,No2; // Characteristics 
+
+    void fun()      //Behavior
+    {
+        cout << "Inside Fun \n"; 
+    }
+
+    void gun()  //Behavior
+    {
+        cout << "Inside gun \n";
+    }
+};
+
+int main()
+{
+    // Object Creation (Instance)
+    Marvellous mobj1;
+    Marvellous mobj2;
+
+    cout << sizeof(mobj1)<<"\n"; //8
+
+    cout << mobj1.No1 <<"\n";
+    
+    mobj1.fun();
+    mobj2.fun();
+
+    mobj1.gun();
+
+    return 0;
+}
