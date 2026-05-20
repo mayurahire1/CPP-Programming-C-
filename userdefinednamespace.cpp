da3e799ce9a9df15b11434648c5612f761ad71c5
Mayur Ahire <162018537+mayurahire1@users.noreply.github.com>
2026-05-20 21:28:23 +0530

    Add files via upload

---
+++ userdefinednamespace.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+namespace Marvellous
+{
+    int no = 11;
+    void fun()
+    {
+        std::cout << "Marvellous";
+    }
+}
+
+int main()
+{
+    std::cout << "Jay Ganesh";
+
+    fun();  // Error
+
+    return 0;
+}
